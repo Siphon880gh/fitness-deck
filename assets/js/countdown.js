1333ffdb1fcfe0e4e78f38eb0eef4c1060394e0f
Siphon880gh <weng.f.fung@gmail.com>
2023-05-02 20:35:04 -0700

    Countdown control bar 5/ - User functional

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -7,47 +7,77 @@ window.countdown = {
 }
 // Set defaults
 window.countdown = {
-    ...countdown, 
+    ...countdown,
     timeAt: 0,
     timeCap: 5
 }
 
-document.addEventListener("DOMContentLoaded", ()=>{
+var poller = () => {
+    console.log("Poller")
+    if (window.countdown.status === "PLAYING") {
+        if (window.countdown.timeAt < window.countdown.timeCap) {
+            window.countdown.timeAt = window.countdown.timeAt + 1;
+            // debugger;
+            document.querySelector("#countdown-display").innerHTML = `${window.countdown.timeAt} / ${window.countdown.timeCap}`;
+
+        } else {
+            document.querySelectorAll("html, #bar-controls").forEach(el => { el.classList.add("countdown-finished"); });
+            if (pollerId)
+                clearInterval(pollerId);
+        }
+
+    }
+}
+pollerId = null;
+document.addEventListener("DOMContentLoaded", () => {
     var display = document.querySelector("#countdown-display")
-    document.querySelectorAll(".countdown-quant, #countdown-display, #countdown-stop-play").forEach(el=>{
-        el.addEventListener("click",()=>{
-                var wasFinished = document.querySelectorAll(".countdown-finished");
-                if(wasFinished) {
-                    wasFinished.forEach(el=> {
-                        el.classList.remove("countdown-finished"); 
-                    });
-                }
+    document.querySelectorAll(".countdown-quant, #countdown-display, #countdown-stop-play").forEach(el => {
+        el.addEventListener("click", () => {
+            var wasFinished = document.querySelectorAll(".countdown-finished");
+            if (wasFinished) {
+                wasFinished.forEach(el => {
+                    el.classList.remove("countdown-finished");
+                });
+            }
 
-                if(el.matches("#countdown-stop-play")) {
+            if (el.matches("#countdown-stop-play")) {
                 window.countdown = {
                     ...countdown,
-                    status: window.countdown.status==="PLAYING"?"STOPPED":"PLAYING"
+                    status: window.countdown.status === "PLAYING" ? "STOPPED" : "PLAYING"
                 }
                 console.log(window.countdown);
 
-            } else if(el.matches("#countdown-display")) {
+                // After wanting Play
+                if (window.countdown.status === "PLAYING") {
+                    window.countdown.timeAt = 0;
+                    pollerId = setInterval(poller, 1000);
+                }
+
+            } else if (el.matches("#countdown-display")) {
                 window.countdown = {
                     ...countdown,
-                    timeAt: window.countdown.status==="PLAYING"?0:window.timeCap,
-                    status: window.countdown.status==="PLAYING"?"PAUSED":"PLAYING"
+                    timeAt: window.countdown.status === "PLAYING" ? 0 : window.timeCap,
+                    status: window.countdown.status === "PLAYING" ? "PAUSED" : "PLAYING"
                 }
                 console.log(window.countdown);
-            } else if(el.matches(".countdown-quant")) {
+
+
+                // After wanting Play
+                if (window.countdown.status === "PLAYING") {
+                    window.countdown.timeAt = 0;
+                    pollerId = setInterval(poller, 1000);
+                }
+            } else if (el.matches(".countdown-quant")) {
                 window.countdown = {
                     ...countdown,
-                    timeCap: (()=>{
+                    timeCap: (() => {
                         var isPlus = document.querySelector("#countdown-operator").classList.contains("is-plus")
                         //console.log({isPlus})
                         var dataValue = el.getAttribute('data-value');
-                        if(isPlus) {
+                        if (isPlus) {
                             return window.countdown.timeCap + parseInt(dataValue);
                         } else {
-                            return window.countdown.timeCap - parseInt(dataValue)<0?0:window.countdown.timeCap - parseInt(dataValue);
+                            return window.countdown.timeCap - parseInt(dataValue) < 0 ? 0 : window.countdown.timeCap - parseInt(dataValue);
                         }
 
                     })()
