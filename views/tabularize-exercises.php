f969d4c5730e5735a268c27aeb8c076d9f41253a
Siphon880gh <weng.f.fung@gmail.com>
2023-05-05 20:28:47 -0700

    UX: More mobile responsiveness at exercise type listing

--- views/tabularize-exercises.php
+++ views/tabularize-exercises.php
@@ -36,7 +36,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>Fitness Deck - <?php echo $_GET["md-file"]; ?></title>
 
     <link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet"/>
